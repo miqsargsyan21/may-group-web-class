3fb3d7bfc178882153440b7d977a8b86d40730fc
miqsargsyan21 <michael.sargs1an@gmail.com>
2021-08-12 10:47:33 +0400

    Added fourth homework

---
+++ js/fourthHomework/script.js
@@ -0,0 +1,7 @@
+let firstNumber = 6,
+    secondNumber = 5;
+
+console.log(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+console.log(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+console.log(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+console.log(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
